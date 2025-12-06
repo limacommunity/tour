7b2fc6433750b1b27c11d34a287dac57a30b3805
limacctech <tech@limacc.com>
2025-12-06 16:44:39 -0500

    Update data.js

--- data.js
+++ data.js
@@ -72,7 +72,7 @@ var projectData = {
 						"type": "link",                        
 						"yaw": 1.6948440686584476,
                         "pitch": 0.40957697755964872,
-                        "rotation": 1.5707963267948966,
+                        "rotation": 0,
                         "target": "3-parking-lot---west",
 						"text": "Go to Parking Lot - West"
                       }
